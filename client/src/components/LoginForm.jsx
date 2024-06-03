7d7a4af1ce81313634bafb60ef3564532907854d
Snalepa <snalepa1998@gmail.com>
2024-06-03 10:15:54 -0400

    Added signup form functionality

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -4,11 +4,14 @@ import { Form, Button, Alert } from 'react-bootstrap';
 
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [loginUser, { error, data }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,7 +29,12 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      // const response = await loginUser(userFormData);
+      const { data } = await loginUser({
+        variables: {...userFormData}
+      });
+
+      Auth.login(data.loginUser.token);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
 
 const SignupForm = () => {
   // set initial form state
@@ -11,6 +13,7 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  const [createUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,7 +31,11 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({
+        variables: { ...userFormData }
+      });
+
+      Auth.login(data.addUser.token);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
